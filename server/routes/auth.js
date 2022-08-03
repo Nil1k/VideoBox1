a77a8835c9bf45dc8b2dd217e54fc5f3058fd0bc
Nilesh Kumar <singhnilk111@gmail.com>
2022-08-03 08:33:36 +0530

    intial

---
+++ server/routes/auth.js
@@ -0,0 +1,11 @@
+import express from "express";
+import {} from "../controllers/auth.js";
+
+const router = express.Router();
+//create a user
+//sign in
+//google authentication
+
+
+
+export default router;
